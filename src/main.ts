00c564c0b93ca7d747ccfd9096e611ca9e4afa10
frivolta <rivoltafilippo@gmail.com>
2021-01-03 17:25:12 +0100

    Adding process env port

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe(),
-  )
-  app.enableCors()
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
